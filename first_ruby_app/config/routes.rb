49d4220091ed9942d53f5b7c9e88d0b823150993
hashmat00 <info@hashmat00.com>
2016-11-05 16:44:11 +0000

    made changes to gem to production ready

--- first_ruby_app/config/routes.rb
+++ first_ruby_app/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  
   root 'welcome#home'
   get 'about', to: 'welcome#about'
   
