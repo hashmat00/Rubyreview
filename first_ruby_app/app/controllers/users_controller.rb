37cc86f3c2e927e57285ee711a54f1c789b5af8e
hashmat00 <info@hashmat00.com>
2016-11-15 22:10:23 +0000

    added edit user page, edit/updae controllers

--- first_ruby_app/app/controllers/users_controller.rb
+++ first_ruby_app/app/controllers/users_controller.rb
@@ -1,36 +1,54 @@
 class UsersController < ApplicationController
 
-def new
-
-@user = User.new
-
-end
-
-def create
-
-@user = User.new(user_params)
-
-if @user.save
-
-flash[:success] = "Welcome to the alpha blog #{@user.username}"
-
-redirect_to articles_path
-
-else
-
-render 'new'
-
-end
-
-end
-
-private
-
-def user_params
-
-params.require(:user).permit(:username, :email, :password)
-
-end
+    def new
+    
+        @user = User.new
+    
+    end
+    
+    
+    def create
+    
+    @user = User.new(user_params)
+    
+        if @user.save
+        
+        flash[:success] = "Welcome to the alpha blog #{@user.username}"
+        
+        redirect_to articles_path
+        
+        else
+        
+        render 'new'
+        
+        end
+    
+    end
+    
+    
+    def edit
+       @user = User.find(params[:id]) 
+    end
+    
+    def update
+       @user = User.find(params[:id])
+       if @user.update(user_params)
+           flash[:success] = "You have successfully updated your account"
+           redirect_to articles_path
+       else
+          render 'new' 
+       end
+        
+    end
+    
+    
+    private
+    
+    def user_params
+    
+    params.require(:user).permit(:username, :email, :password)
+    
+    end
 
 end
 
