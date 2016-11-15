f4036e1b0662ca83a3cb0d2c4054c9b4b7de63b9
hashmat00 <info@hashmat00.com>
2016-11-15 22:46:15 +0000

    added user show page, with user article, user avatar, added user gravatar method on helper controller

--- first_ruby_app/app/controllers/users_controller.rb
+++ first_ruby_app/app/controllers/users_controller.rb
@@ -42,6 +42,13 @@ class UsersController < ApplicationController
     end
     
     
+    def show
+     @user = User.find(params[:id])    
+    end
+    
+    
+    
+    
     private
     
     def user_params
